2b63a2ae21a2747038016a7da124dabb660ea86f
Alkesst <alec.augal@gmail.com>
2019-04-23 16:36:45 +0200

    Fixed errors in the RSA class. Also finished implemented the "protocol" of communication between clients.

--- practica3/main_test.py
+++ practica3/main_test.py
@@ -1,20 +1,40 @@
+__author__ = "Alejandro Garau Madrigal"
 from practica3.rsa import RSAObject
 
 RELATIVE_PATH = '/Users/alec/Desktop/{}'
 
 
 def main():
-    rsa = RSAObject()
-    rsa.create_key_pair()
-    rsa.load_private_key(RELATIVE_PATH.format("private.pem"), "123456")
-    rsa.load_public_key(RELATIVE_PATH.format("public.pem"))
-    a = rsa.cipher("Vaya poya que tienes compae XD")
-    print(a)
-    b = rsa.decipher(a)
-    print(b)
-    a = rsa.sign("El mendizábal")
-    print(a)
-    print(rsa.check_sign("El mendizábal", a))
+    a = RSAObject()
+    print("Created client A")
+    b = RSAObject()
+    print("Created client B")
+    b.create_key_pair()
+    print("Generated a key pair for the client B")
+    b.save_public_key(RELATIVE_PATH.format("b_public.pem"))
+    b.save_private_key(RELATIVE_PATH.format("b_private.pem"), "1234")
+    print("Saved the keys for client b in {}".format(RELATIVE_PATH.format("")))
+    a.create_key_pair()
+    print("Generated a key pair for the client A")
+    a.save_public_key(RELATIVE_PATH.format("a_public.pem"))
+    a.save_private_key(RELATIVE_PATH.format("a_private.pem"), "1234")
+    print("Saved the keys for client a in {}".format(RELATIVE_PATH.format("")))
+
+    # Ciframos el mensaje:
+
+    ciphered = b.cipher("Hola Amigos de la Seguridad")
+    print("A ciphered the message using the public key of client B.")
+    print(ciphered)
+
+    s = a.sign(ciphered)
+    print("Client A signed the content using his own public key.")
+    print("Sending ciphered and signed text to B client...")
+    # Se lo """""""""mandamos""""""""" al cliente B.
+    if a.check_sign(ciphered, s):
+        print("The sign is correct. Client A has sent this information.\n"
+              "Deciphered message: {}".format(b.decipher(ciphered)))
+    else:
+        print("The sender of this content is not the client A. Untrusted source.\nAborting...")
 
 
 if __name__ == '__main__':
--- practica3/rsa.py
+++ practica3/rsa.py
@@ -3,6 +3,8 @@ from Crypto.Cipher import PKCS1_OAEP
 from Cryptodome.Hash import SHA256
 from Crypto.Signature import pss
 
+__author__ = "Alejandro Garau Madrigal"
+
 
 class RSAObject(object):
     def __init__(self):
@@ -16,7 +18,7 @@ class RSAObject(object):
     def create_key_pair(self):
         """Crea un par de claves publico/privada, y las almacena dentro de la instancia"""
         self.__key = RSA.generate(2048)
-        self.__private_key = self.__key.exportKey
+        self.__private_key = self.__key  # va, que es sin el .export_key. El self.__key es en sí la clave privada nice.
         self.__public_key = self.__key.publickey()
 
     def save_private_key(self, file, password):
@@ -66,7 +68,6 @@ class RSAObject(object):
         except ValueError:
             return None
 
-
     def sign(self, datos):
         """Firma el parámetro datos (de tipo binario) con la clave self.private_key, y devuelve
        el resultado. En caso de error, se devuelve None."""
@@ -74,18 +75,20 @@ class RSAObject(object):
             datos = datos.encode("utf-8")
         hashed = SHA256.new(datos)  # Ya veremos los hash la semana que viene
         print(hashed.hexdigest())
-        signature = pss.new(self.private_key).sign(hashed)
+        signature = pss.new(self.__private_key).sign(hashed)
         return signature
 
-
     def check_sign(self, text, signature):
-        """Comprueba el parámetro text (de tipo binario) con respecto a una firma signature
+        """Comprueba el :param text: (de tipo binario) con respecto a una firma signature
         (de tipo binario), usando para ello la clave self.public_key.
-        Devuelve True si la comprobacion es correcta, o False en caso contrario o
-        en caso de error."""
+        :param text: texto a comprobar
+        :param signature: firma
+        :return: True si la comprobacion es correcta, o False en caso contrario o
+        en caso de error.
+        """
         if type(text) != bytes:
             text = text.encode("utf-8")
-        hashed = SHA256.new(text)  # Ya veremos los hash la semana que viene
+        hashed = SHA256.new(text)
         print(hashed.hexdigest())
         verifier = pss.new(self.public_key)
         try:
